31561f4dc5fb215c517da69c8dc65d18923911cf
Madhuri <madhurikbl.edp@gmail.com>
2025-09-10 19:03:51 +0530

    Added Resume & Job APIs, SkillGapController, and started frontend setup

---
+++ src/test/java/com/resumematcher/skillgap_analyzer/SkillgapAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resumematcher.skillgap_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillgapAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
